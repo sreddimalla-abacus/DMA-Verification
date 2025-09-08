df5b10faf996fb7898fab955c87d4534fb8fe861
Sandeep Rao Reddimalla <sandeep.reddimalla@abacus-semi.com>
2025-09-08 07:34:31 -0700

    Create dma_tb_simple.sv

---
+++ dma_tb_simple.sv
@@ -0,0 +1,25 @@
+module top;
+	
+timeunit 1ns;
+timeprecision 1ns;
+	
+bit CLK, RESET;
+	
+dma_if.TB dif(CLK, RESET);
+	
+dma_top DMA_TOP (dif);
+
+// 5 Mhz clock ie 200 ns time period
+initial begin
+forever #100  CLK = ~CLK; 
+end
+
+// Apply and life reset
+initial begin
+	repeat(10)@(negedge CLK); RESET = 1;
+	repeat(100)@(negedge CLK); RESET = 0;
+end
+
+endmodule
+
+
